b02f92463a21ed25185c837debcd01a559656104
ECast1llo <ecastillo@applaudostudios.com>
2021-06-08 14:58:03 -0600

    Update APIMain.java

--- RestAsuredBB/src/main/java/APIMain.java
+++ RestAsuredBB/src/main/java/APIMain.java
@@ -11,8 +11,7 @@ public class APIMain extends RestAssuredBase{
         Character[] characters = response.jsonPath().getObject("",Character[].class);
         for (Character cr:characters){
             if (cr.getChar_id().equals("1")){
-                System.out.println(cr.getBirthday());
-                System.out.println("\n");
+                System.out.println(cr.getBirthday()+"\n \n");
             }
             System.out.println("Character name: " + cr.getName());
             System.out.println("Portrayed:  " + cr.getPortrayed());
